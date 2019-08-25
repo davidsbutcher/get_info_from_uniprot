24ec0a1cb90c1fff28c7bb62b5585e0f7d84244a
drdavidbutcher <52331079+drdavidbutcher@users.noreply.github.com>
2019-08-25 19:16:37 -0400

    Added package installer

---
+++ A_install_packages.R
@@ -0,0 +1,37 @@
+if(!require(here)) install.packages("here")
+
+if(!require(svMisc)) install.packages("svMisc")
+
+if(!require(furrr)) install.packages("furrr")
+
+if(!require(Peptides)) install.packages("Peptides")
+
+if(!require(magrittr)) install.packages("magrittr")
+
+if(!require(writexl)) install.packages("writexl")
+
+if(!require(readxl)) install.packages("readxl")
+
+if(!require(RSQLite)) install.packages("RSQLite")
+
+if(!require(tools)) install.packages("tools")
+
+if(!require(progress)) install.packages("progress")
+
+if(!require(tictoc)) install.packages("tictoc")
+
+if(!require(glue)) install.packages("glue")
+
+if(!require(RPushbullet)) install.packages("RPushbullet")
+
+if(!require(ggpubr)) install.packages("ggpubr")
+
+if(!require(tidyverse)) install.packages("tidyverse")
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+if(!require(GO.db)) BiocManager::install("GO.db")
+
+if(!require(UniProt.ws)) BiocManager::install("UniProt.ws")
+
